6d69e84b32e2f71d9d56cc6aa9b142484c499ab2
Gustavo Rondello <gustavorondello99@outlook.com>
2023-02-06 16:07:10 -0300

    feat: função remove do arquivo file_process.py implementada

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -22,7 +22,12 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance or len(instance) == 0:
+        return print("Não há elementos")
+    
+    file = instance.dequeue()
+    
+    print(f"Arquivo {file['nome_do_arquivo']} removido com sucesso")
 
 
 def file_metadata(instance, position):
