26ec20cad535255e80a9142df077b47bec8ac4d0
Gustavo Rondello <gustavorondello99@outlook.com>
2023-02-07 16:45:35 -0300

    feat: função exists_word do arquivo word_search.py implementada

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    search = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        line = 0
+
+        output = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for item in file["linhas_do_arquivo"]:
+            line += 1
+            if word.lower() in item.lower():
+                output["ocorrencias"].append({"linha": line})
+
+        if output["ocorrencias"]:
+            search.append(output)
+
+    return search
 
 
 def search_by_word(word, instance):
